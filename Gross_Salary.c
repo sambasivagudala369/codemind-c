c113c83c67d92ae0a54ea3a81a294dafe6f881a4
sambasivagudala369 <119730311+sambasivagudala369@users.noreply.github.com>
2023-01-25 18:58:32 +0530

    #include<stdio.h> int main() {     int b,d,h;     scanf("%d",&b);     float g;     if(b<=10000)     {         d=b*0.8;         h=0.2*b;         g=b+d+h;     }     else if(b<=20000)     {         d=0.9*b;         h=0.25*b;         g=b+d+h;     }     else if(b>20000)     {         d=0.95*b;         h=0.3*b;         g=b+d+h;     }     printf("%.2f",g); }

--- Gross_Salary.c
+++ Gross_Salary.c
@@ -1,27 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int a,d,h;
-    scanf("%d",&a);
+    int b,d,h;
+    scanf("%d",&b);
     float g;
-    if(a<=10000)
+    if(b<=10000)
     {
-        d=a*0.8;
-        h=a*0.2;
-        g=a+d+h;
+        d=b*0.8;
+        h=0.2*b;
+        g=b+d+h;
     }
-    else if(a<=20000)
+    else if(b<=20000)
     {
-        d=a*0.9;
-        h=a*0.25;
-        g=a+d+h;
+        d=0.9*b;
+        h=0.25*b;
+        g=b+d+h;
     }
-    else
+    else if(b>20000)
     {
-        d=a*0.95;
-        h=a*0.3;
-        g=a+d+h;
+        d=0.95*b;
+        h=0.3*b;
+        g=b+d+h;
     }
     printf("%.2f",g);
-    
 }
